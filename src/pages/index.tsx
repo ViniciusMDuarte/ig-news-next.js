1cd512cb787505e4d84db51100c29704bfe6498d
ViniciusMDuarte <viniduarte222@gmail.com>
2021-04-04 09:30:39 -0300

    created page main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-          <title> ig.news | Início </title>
+          <title> ig.news | Home </title>
       </Head>
+
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, Welcome</span>
+
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img  src='/images/avatar.svg' alt='Girl coding' />
+
+      </main>
+
+
    </>
   )
 }
